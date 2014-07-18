304f5753c9f7579d5099fb403fb359cec104bc04
switchback_sus4 <qurumiharukaze@gmail.com>
2014-07-18 22:34:58 +0900

    first commit

---
+++ downloader_curl.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+
+curl -O http://125.6.169.35/idolmaster/image_sp/event_flash/413/idol/idol_[1-4]_0_1_[1-5].png
---
+++ downloader_wget.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+for i in 1 2 3 4
+do
+ for j in 1 2 3 4 5
+ do
+  wget http://125.6.169.35/idolmaster/image_sp/event_flash/413/idol/idol_${i}_0_1_${j}.png
+ done
+done
